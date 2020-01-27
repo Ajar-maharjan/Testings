f484f843f1130653e5e7f076d224d59cbad14b99
Ajar-maharjan <pleiades.ajar@gmail.com>
2020-01-27 14:47:53 +0545

    N OMEGALUL

--- app/src/test/java/com/pepo/unittesting/myTest.java
+++ app/src/test/java/com/pepo/unittesting/myTest.java
@@ -1,14 +1,21 @@
 package com.pepo.unittesting;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class myTest {
+    Arithmetic arithmetic = new Arithmetic();
 
+    @Before
+    public void test(){
+        arithmetic = new Arithmetic();
+    }
     @Test
     public void Add() {
-        Arithmetic arithmetic = new Arithmetic();
+
         float actual = arithmetic.add(2, 3);
         float expected = 5;
         assertEquals(expected, actual);
@@ -16,16 +23,19 @@ public class myTest {
 
     @Test
     public void Sub() {
-        Arithmetic arithmetic = new Arithmetic();
         float actual = arithmetic.subtract(5, 4);
         float expected = 1;
         assertEquals(expected, actual);
     }
     @Test
     public void Div() {
-        Arithmetic arithmetic = new Arithmetic();
         float actual = arithmetic.divide(12, 2);
         float expected = 6;
         assertEquals(expected, actual);
+    }
+
+    @After
+    public void aftertest(){
+        arithmetic = null;
     }
 }
