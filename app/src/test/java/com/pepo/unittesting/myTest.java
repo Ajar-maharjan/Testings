6f411ccd4781ddbee37ab3799c779e70a6dbbb90
Ajar-maharjan <pleiades.ajar@gmail.com>
2020-01-27 14:44:52 +0545

    Hyaaaa

--- app/src/test/java/com/pepo/unittesting/myTest.java
+++ app/src/test/java/com/pepo/unittesting/myTest.java
@@ -7,10 +7,25 @@ import static junit.framework.TestCase.assertEquals;
 public class myTest {
 
     @Test
-    public void checkTwoNo() {
+    public void Add() {
         Arithmetic arithmetic = new Arithmetic();
         float actual = arithmetic.add(2, 3);
         float expected = 5;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Sub() {
+        Arithmetic arithmetic = new Arithmetic();
+        float actual = arithmetic.subtract(5, 4);
+        float expected = 1;
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void Div() {
+        Arithmetic arithmetic = new Arithmetic();
+        float actual = arithmetic.divide(12, 2);
+        float expected = 6;
         assertEquals(expected, actual);
     }
 }
